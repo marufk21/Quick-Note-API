82a2f2dc0dd420b4ccb3bf76b6f6a82d3e395a05
Maruf Khan <marufkhan2002@gmail.com>
2023-05-30 18:46:59 +0530

    update-auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { body, validationResult } = require("express-validator");
-const fetchuser = require('../middleware/fetchUser');
+const fetchuser = require("../middleware/fetchUser");
 
 const JWT_SECRET = "marufisagoodb$oy";
 
@@ -14,49 +14,52 @@ router.post(
   [
     body("name", "Enter a valid name").isLength({ min: 3 }),
     body("email", "Enter a valid email").isEmail(),
-    body("password", "Password must be atleast 5 characters").isLength({
+    body("password", "Password must be at least 5 characters").isLength({
       min: 5,
     }),
   ],
   async (req, res) => {
     let success = false;
-    // If there are errors, return Bad request and the errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({success, errors: errors.array() });
-    }
-    // Check whether the user with this email exists already
     try {
-      let user = await User.findOne({ email: req.body.email });
-      if (user) {
-        return res
-          .status(400)
-          .json({
-            success,
-            error: "Sorry a user with this email already exists",
-          });
+      // If there are errors, return Bad request and the errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ success: false, errors: errors.array() });
       }
 
-      //Hashing a password using bcryptjs
+      const { name, email, password } = req.body;
+
+      // Check whether the user with this email exists already
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({
+          success,
+          error: "Sorry, a user with this email already exists",
+        });
+      }
+
+      // Hash the password using bcryptjs
       const salt = await bcrypt.genSalt(10);
-      const securepass = await bcrypt.hash(req.body.password, salt);
+      const hashedPassword = await bcrypt.hash(password, salt);
 
       // Create a new user
-      user = await User.create({
-        name: req.body.name,
-        password: securepass,
-        email: req.body.email,
+      user = new User({
+        name,
+        email,
+        password: hashedPassword,
       });
 
-      //JWT
-      const data = {
+      await user.save();
+
+      // Generate JWT
+      const payload = {
         user: {
           id: user.id,
         },
       };
-      const authtoken = jwt.sign(data, JWT_SECRET);
-      // res.json(user)
-      success = true
+      const authtoken = jwt.sign(payload, JWT_SECRET);
+
+      success = true;
       res.json({ success, authtoken });
     } catch (error) {
       console.error(error.message);
@@ -74,37 +77,40 @@ router.post(
   ],
   async (req, res) => {
     let success = false;
-    // If there are errors, return Bad request and the errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    const { email, password } = req.body;
     try {
-      let user = await User.findOne({ email });
+      // If there are errors, return Bad request and the errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const { email, password } = req.body;
+
+      // Find the user by email
+      const user = await User.findOne({ email });
       if (!user) {
-        success = false;
         return res
           .status(400)
           .json({ error: "Please try to login with correct credentials" });
       }
 
-      const passwordCompare = await bcrypt.compare(password, user.password);
-      if (!passwordCompare) {
-        success = false;
+      // Compare passwords
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
         return res.status(400).json({
           success,
           error: "Please try to login with correct credentials",
         });
       }
 
-      const data = {
+      // Generate JWT
+      const payload = {
         user: {
           id: user.id,
         },
       };
-      const authtoken = jwt.sign(data, JWT_SECRET);
+      const authtoken = jwt.sign(payload, JWT_SECRET);
+
       success = true;
       res.json({ success, authtoken });
     } catch (error) {
@@ -114,11 +120,10 @@ router.post(
   }
 );
 
-// ROUTE 3: Get loggedin User Details using: POST "/api/auth/getuser". Login required
+// ROUTE 3: Get logged-in User Details using: POST "/api/auth/getuser". Login required
 router.post("/getuser", fetchuser, async (req, res) => {
   try {
-    userId = req.user.id;
-    const user = await User.findById(userId).select("-password");
+    const user = await User.findById(req.user.id).select("-password");
     res.send(user);
   } catch (error) {
     console.error(error.message);
